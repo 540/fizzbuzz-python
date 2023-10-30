bd2e61ac9db659c39b1359d5314f6693ab7eb045
Gorka Moreno Asín <gorka@540deg.com>
2023-10-30 11:14:42 +0100

    One to many open close]

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,32 +1,19 @@
 from collections.abc import Callable
-from src.predicate import is_multiple_of, and_p, or_p, contains
+from dataclasses import dataclass
 
 
-def fizzbuzz() -> int:
-    rule_set = [
-        [
-            and_p(is_multiple_of(3), is_multiple_of(5)),
-            lambda number: "FizzBuzz",
-        ],
-        [
-            or_p(is_multiple_of(3), contains(3)),
-            lambda number: "Fizz",
-        ],
-        [
-            or_p(is_multiple_of(5), contains(5)),
-            lambda number: "Buzz",
-        ],
-    ]
+@dataclass
+class FizzBuzz:
+    rule_set: list[tuple[Callable[[int], bool], Callable[[int], str]]]
+    numbers: list[int]
 
-    return [singleFizzbuzz(rule_set, n) for n in range(1, 101)]
+    def run(self):
+        return [self._run_single(number) for number in self.numbers]
 
+    def _run_single(self, number: int):
+        apply_rule = next(
+            (predicate[1] for predicate in self.rule_set if predicate[0](number)),
+            str,
+        )
 
-def singleFizzbuzz(
-    rule_set: list[tuple[Callable[[int], bool], Callable[[int], str]]], number: int
-) -> str:
-    apply_rule = next(
-        (predicate[1] for predicate in rule_set if predicate[0](number)),
-        str,
-    )
-
-    return apply_rule(number)
+        return apply_rule(number)
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,50 +1,72 @@
 from hamcrest import assert_that, has_length, is_
-from src.fizzbuzz import fizzbuzz
+from pytest import fixture
+from src.predicate import and_p, is_multiple_of, contains, or_p
+from src.fizzbuzz import FizzBuzz
 
 
-def test_returns_1_string_for_1_number():
-    assert_that(fizzbuzz_number(1), is_("1"))
+@fixture(name="fizz_buzz")
+def fixgure_fizz_buzz() -> FizzBuzz:
+    rule_set = [
+        [
+            and_p(is_multiple_of(3), is_multiple_of(5)),
+            lambda number: "FizzBuzz",
+        ],
+        [
+            or_p(is_multiple_of(3), contains(3)),
+            lambda number: "Fizz",
+        ],
+        [
+            or_p(is_multiple_of(5), contains(5)),
+            lambda number: "Buzz",
+        ],
+    ]
+
+    return FizzBuzz(rule_set, range(1, 101))
 
 
-def test_returns_2_string_for_2_number():
-    assert_that(fizzbuzz_number(2), is_("2"))
+def test_returns_1_string_for_1_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 1), is_("1"))
 
 
-def test_returns_Fizz_for_3_number():
-    assert_that(fizzbuzz_number(3), is_("Fizz"))
+def test_returns_2_string_for_2_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 2), is_("2"))
 
 
-def test_returns_Fizz_for_6_number():
-    assert_that(fizzbuzz_number(6), is_("Fizz"))
+def test_returns_Fizz_for_3_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 3), is_("Fizz"))
 
 
-def test_returns_Buzz_for_5_number():
-    assert_that(fizzbuzz_number(5), is_("Buzz"))
+def test_returns_Fizz_for_6_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 6), is_("Fizz"))
 
 
-def test_returns_Buzz_for_10_number():
-    assert_that(fizzbuzz_number(10), is_("Buzz"))
+def test_returns_Buzz_for_5_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 5), is_("Buzz"))
 
 
-def test_returns_FizzBuzz_for_15_number():
-    assert_that(fizzbuzz_number(15), is_("FizzBuzz"))
+def test_returns_Buzz_for_10_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 10), is_("Buzz"))
 
 
-def test_returns_Fizz_for_number_containing_3():
-    assert_that(fizzbuzz_number(13), is_("Fizz"))
+def test_returns_FizzBuzz_for_15_number(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 15), is_("FizzBuzz"))
 
 
-def test_returns_Buzz_for_number_containing_5():
-    assert_that(fizzbuzz_number(52), is_("Buzz"))
+def test_returns_Fizz_for_number_containing_3(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 13), is_("Fizz"))
 
 
-def test_returns_multiple_numbers():
-    assert_that(fizzbuzz()[:5], is_(["1", "2", "Fizz", "4", "Buzz"]))
+def test_returns_Buzz_for_number_containing_5(fizz_buzz: FizzBuzz):
+    assert_that(fizzbuzz_number(fizz_buzz, 52), is_("Buzz"))
 
 
-def test_returns_100_numbers():
-    assert_that(fizzbuzz(), has_length(100))
+def test_returns_multiple_numbers(fizz_buzz: FizzBuzz):
+    assert_that(fizz_buzz.run()[:5], is_(["1", "2", "Fizz", "4", "Buzz"]))
 
 
-def fizzbuzz_number(number: int):
-    return fizzbuzz()[number - 1]
+def test_returns_100_numbers(fizz_buzz: FizzBuzz):
+    assert_that(fizz_buzz.run(), has_length(100))
+
+
+def fizzbuzz_number(fizz_buzz, number: int):
+    return fizz_buzz.run()[number - 1]
