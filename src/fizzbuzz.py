84c995b57ea40b55382c68ada04a64dfc1fb0d7e
Gorka Moreno Asín <gorka@540deg.com>
2023-10-30 08:30:26 +0100

    Solved with basic requeriments and single value

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,2 +1,21 @@
-def fizzbuzz() -> int:
-    return
+def fizzbuzz(number: int) -> int:
+    if _is_multiple_of_5(number) and _is_multiple_of_3(number):
+        return "FizzBuzz"
+    if _is_multiple_of_3(number):
+        return "Fizz"
+    if _is_multiple_of_5(number):
+        return "Buzz"
+
+    return str(number)
+
+
+def _is_multiple_of(multiple: int) -> int:
+    return lambda number: number % multiple == 0
+
+
+def _is_multiple_of_3(number: int) -> int:
+    return _is_multiple_of(3)(number)
+
+
+def _is_multiple_of_5(number: int) -> int:
+    return _is_multiple_of(5)(number)
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -2,5 +2,29 @@ from hamcrest import assert_that, is_
 from src.fizzbuzz import fizzbuzz
 
 
-def test_fizzbuzz_example():
-    assert_that(fizzbuzz(), is_(None))
+def test_returns_1_string_for_1_number():
+    assert_that(fizzbuzz(1), is_("1"))
+
+
+def test_returns_2_string_for_2_number():
+    assert_that(fizzbuzz(2), is_("2"))
+
+
+def test_returns_Fizz_for_3_number():
+    assert_that(fizzbuzz(3), is_("Fizz"))
+
+
+def test_returns_Fizz_for_6_number():
+    assert_that(fizzbuzz(6), is_("Fizz"))
+
+
+def test_returns_Buzz_for_5_number():
+    assert_that(fizzbuzz(5), is_("Buzz"))
+
+
+def test_returns_Buzz_for_10_number():
+    assert_that(fizzbuzz(10), is_("Buzz"))
+
+
+def test_returns_FizzBuzz_for_15_number():
+    assert_that(fizzbuzz(15), is_("FizzBuzz"))
